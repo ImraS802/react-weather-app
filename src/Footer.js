4d8ec39a0ebb0d0c197b4d7975b21ba7073bb122
ImraS802 <68655356+ImraS802@users.noreply.github.com>
2020-10-07 21:02:01 +0200

    Initial commit

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <small>
+        <a
+          href="https://github.com/ImraS802/react-weather-app"
+          target="_blank"
+          class="open-source"
+        >
+          <em>Open-source code </em>
+        </a>
+        <em>by Imra S.</em>
+      </small>
+    </div>
+  );
+}
---
+++ src/Weather.js
@@ -0,0 +1,93 @@
+import React from "react";
+
+import "./Weather.css";
+
+export default function Weather() {
+  let weatherData = {
+    city: "New York",
+    temperature: 26,
+    date: "Sunday, 08:15",
+    description: "Sunny",
+    imgUrl:
+      "https://seekpng.com/png/detail/2-22841_emoji-sun-and-png-image-transparent-background-sun.png",
+    humidity: 20,
+    wind: 3,
+  };
+
+  return (
+    <div className="weather-app-wrapper">
+      <div className="Weather">
+        <form className="form-inline" id="search-form">
+          <div className="form-group mb-2">
+            <label for="staticCity2" className="sr-only"></label>
+          </div>
+          <div className="form-group mx-sm-3 mb-2">
+            <label for="inputCity2" className="sr-only"></label>
+            <input
+              type="text"
+              className="form-control shadow"
+              id="input-city"
+              placeholder="Enter a city"
+              autocomplete="off"
+              autofocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <button type="submit" className="btn btn-primary mb-2 shadow">
+              Search
+            </button>
+          </div>
+
+          <button
+            className="btn btn-success w-200 shadow"
+            id="current-location-button"
+          >
+            via GPS
+          </button>
+        </form>
+        <br />
+        <h1>{weatherData.city}</h1>
+        <div className="weather-data">
+          <div className="row">
+            <div className="col-7">
+              <div className="float-left">
+                <ul>
+                  <li>Last updated: {weatherData.date}</li>
+                  <li>{weatherData.description}</li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-5">
+              <ul>
+                <li>Humidity: {weatherData.humidity}% </li>
+                <li>Wind: {weatherData.wind} km/h</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-6">
+          <div className="weather-icon">
+            <div className="row">
+              <img
+                src={weatherData.imgUrl}
+                alt={weatherData.description}
+                className="float-left"
+              />
+            </div>
+          </div>
+          <div className="temperature">{weatherData.temperature}</div>
+          <span className="units">
+            <a href="/">
+              <strong>°C</strong>
+            </a>{" "}
+            |{" "}
+            <a href="/">
+              <strong>°F</strong>
+            </a>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
