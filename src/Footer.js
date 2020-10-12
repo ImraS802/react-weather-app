a4f98e605bfa9f7d2c44f97caea814750289e3db
ImraS802 <68655356+ImraS802@users.noreply.github.com>
2020-10-12 17:39:07 +0200

    Changed text in footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,14 +4,14 @@ export default function Footer() {
   return (
     <div className="Footer">
       <small>
+        <em>This project is coded by Imra S. and is </em>
         <a
           href="https://github.com/ImraS802/react-weather-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <em>Open-sourced on GitHub </em>
+          <em>open-sourced on GitHub </em>
         </a>
-        <em>by Imra S.</em>
       </small>
     </div>
   );
