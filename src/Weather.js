f711580b74f117866d38c9d359722528daddf94d
ImraS802 <68655356+ImraS802@users.noreply.github.com>
2020-10-18 15:22:40 +0200

    Fixed url

--- src/Weather.js
+++ src/Weather.js
@@ -32,7 +32,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "32f74cfde224610d71a804af24f60879";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
